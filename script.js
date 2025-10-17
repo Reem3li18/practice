b091d38703ff81fa92c240ab6131d1f8bc53bb91
Reem3li18 <202404156@student.polytechnic.bh>
2025-10-17 18:24:29 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactform").onsubmit=function(){alert("Your inquiry has been submitted.");
+return false;                                       
+  };
+document.getElementById("tr")[1].onclick=function(){alert("Cupcake sold out");
+  };
